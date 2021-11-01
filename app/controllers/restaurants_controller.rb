aaa5de7cf4f3cb2ab6fbef88a708398544a05a56
rdkulp <rdkulp@gmail.com>
2021-11-01 14:08:27 -0600

    manually created record in db using controller and tested with curl

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,15 @@
 class RestaurantsController < ApplicationController
   def index
+    render json: Restaurant.all
+  end
+
+  def create
+    restaurant = Restaurant.new(name: 'Vinh Xuong', address: 'Zuni', cuisine: 'Viatnamese')
+
+    if restaurant.save
+      render json: restaurant, status: :created
+    else
+      render json: restaurant.errors, status: :unproccessable_entity
+    end
   end
 end
