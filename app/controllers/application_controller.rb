8ed02220d8a570e1808b02e04af5ca1959117231
rdkulp <rdkulp@gmail.com>
2021-11-09 08:15:59 -0700

    rip out devise and doorkeeper to prepare to implement API key authentication without

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,2 @@
 class ApplicationController < ActionController::API
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :doorkeeper_authorize!
-  
-  respond_to :json
-
-  protected
-
-  # Devise methods
-  # Authentication key(:username) and password field will be added automatically by devise.
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit :sign_up, keys: :email
-    devise_parameter_sanitizer.permit :account_update, keys: :email
-  end
-
-  private
-
-  # Doorkeeper methods
-  def current_resource_owner
-    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 
 Rails.application.routes.draw do
-  use_doorkeeper do
-    skip_controllers :applicaitons, :authorized_applications
-  end
-  scope module: :api, defaults: { format: :json }, path: 'api' do
-    scope module: :v1 do
-      devise_for :users, controllers: {
-           registrations: 'api/v1/users/registrations',
-       }, skip: [:sessions, :password]
-    end
-  end
+  resources :restaurants
 
-  authenticated :user do
-    resources :restaurants
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
