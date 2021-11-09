6ada821c438244f81d9053b9ce159c1d2d1d7d79
rdkulp <rdkulp@gmail.com>
2021-11-09 06:37:15 -0700

    put restaurants inside auth

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
        }, skip: [:sessions, :password]
     end
   end
+
+  authenticated :user do
+    resources :restaurants
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
 end
